c96acd19c66d7a656a2fdac9846ae8635b19f442
Renzo Crisanto <117412799+xXRenzoElProXx@users.noreply.github.com>
2025-06-09 22:19:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,172 +1,129 @@
+let currentTransposition = 0;
+let useEnglishNotation = true;
+
 const englishChords = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 const latinChords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si'];
 
-const englishFlats = {
-    'C#': 'Db', 'D#': 'Eb', 'F#': 'Gb', 'G#': 'Ab', 'A#': 'Bb'
-};
+const chordPattern = /\b(Do#|Re#|Fa#|Sol#|La#|Do|Re|Mi|Fa|Sol|La|Si|C#|D#|F#|G#|A#|C|D|E|F|G|A|B)(m|maj|min|sus|dim|aug|\d)*\b/g;
 
-const latinFlats = {
-    'Do#': 'Reb', 'Re#': 'Mib', 'Fa#': 'Solb', 'Sol#': 'Lab', 'La#': 'Sib'
-};
+function getChordArray() {
+    return useEnglishNotation ? englishChords : latinChords;
+}
 
-let currentTransposition = 0;
-let originalText = '';
-let useLatinNotation = false;
-
-function normalizeChord(chord) {
-    const flatsToSharps = useLatinNotation ?
-        { 'Reb': 'Do#', 'Mib': 'Re#', 'Solb': 'Fa#', 'Lab': 'Sol#', 'Sib': 'La#' } :
-        { 'Db': 'C#', 'Eb': 'D#', 'Gb': 'F#', 'Ab': 'G#', 'Bb': 'A#' };
-    return flatsToSharps[chord] || chord;
+function getAlternateChordArray() {
+    return useEnglishNotation ? latinChords : englishChords;
 }
 
 function transposeChord(chord, semitones) {
-    const currentScale = useLatinNotation ? latinChords : englishChords;
+    const chords = getChordArray();
+    const alternateChords = getAlternateChordArray();
 
-    const chordRegex = useLatinNotation ?
-        /^(Do|Re|Mi|Fa|Sol|La|Si)([#b]?)(.*)$/ :
-        /^([A-G])([#b]?)(.*)$/;
+    let rootNote = chord.match(/^(Do#|Re#|Fa#|Sol#|La#|Do|Re|Mi|Fa|Sol|La|Si|C#|D#|F#|G#|A#|C|D|E|F|G|A|B)/)[0];
+    let suffix = chord.replace(rootNote, '');
 
-    const match = chord.match(chordRegex);
-
-    if (!match) return chord;
-
-    let [, root, accidental, extension] = match;
-    let fullRoot = root + accidental;
-    fullRoot = normalizeChord(fullRoot);
-
-    const rootIndex = currentScale.indexOf(fullRoot);
-    if (rootIndex === -1) return chord;
-
-    const newIndex = (rootIndex + semitones + 12) % 12;
-    const newRoot = currentScale[newIndex];
-
-    return newRoot + extension;
-}
-
-function transposeText(text, semitones) {
-    const chordPattern = useLatinNotation ?
-        /\b(Do|Re|Mi|Fa|Sol|La|Si)([#b]?)((?:maj|min|m|M|dim|aug|sus|add|\d)*(?:[#b]?\d+)*)\b/g :
-        /\b([A-G])([#b]?)((?:maj|min|m|M|dim|aug|sus|add|\d)*(?:[#b]?\d+)*)\b/g;
-
-    return text.replace(chordPattern, (match, root, accidental, extension) => {
-        const fullChord = root + accidental + extension;
-        return transposeChord(fullChord, semitones);
-    });
-}
-
-function convertNotation(text, toLatin) {
-    if (toLatin) {
-        let convertedText = text;
-        const conversions = {
-            'C': 'Do', 'D': 'Re', 'E': 'Mi', 'F': 'Fa', 'G': 'Sol', 'A': 'La', 'B': 'Si',
-            'Db': 'Reb', 'Eb': 'Mib', 'Gb': 'Solb', 'Ab': 'Lab', 'Bb': 'Sib'
-        };
-
-        for (const [eng, lat] of Object.entries(conversions)) {
-            const regex = new RegExp(`\\b${eng}([#b]?)((?:maj|min|m|M|dim|aug|sus|add|\\d)*(?:[#b]?\\d+)*)\\b`, 'g');
-            convertedText = convertedText.replace(regex, `${lat}$1$2`);
+    let currentIndex = chords.indexOf(rootNote);
+    if (currentIndex === -1) {
+        currentIndex = alternateChords.indexOf(rootNote);
+        if (currentIndex !== -1) {
+            rootNote = chords[currentIndex];
         }
-
-        return convertedText;
-    } else {
-        let convertedText = text;
-        const conversions = {
-            'Do': 'C', 'Re': 'D', 'Mi': 'E', 'Fa': 'F', 'Sol': 'G', 'La': 'A', 'Si': 'B',
-            'Reb': 'Db', 'Mib': 'Eb', 'Solb': 'Gb', 'Lab': 'Ab', 'Sib': 'Bb'
-        };
-
-        for (const [lat, eng] of Object.entries(conversions)) {
-            const regex = new RegExp(`\\b${lat}([#b]?)((?:maj|min|m|M|dim|aug|sus|add|\\d)*(?:[#b]?\\d+)*)\\b`, 'g');
-            convertedText = convertedText.replace(regex, `${eng}$1$2`);
-        }
-
-        return convertedText;
     }
+
+    if (currentIndex !== -1) {
+        let newIndex = (currentIndex + semitones + 12) % 12;
+        return chords[newIndex] + suffix;
+    }
+
+    return chord;
 }
 
 function processText() {
     const inputText = document.getElementById('inputText').value;
-    originalText = inputText;
+    const outputElement = document.getElementById('outputText');
 
     if (!inputText.trim()) {
-        document.getElementById('outputText').innerHTML = 'El texto transpuesto aparecerá aquí automáticamente...';
+        outputElement.textContent = 'El texto transpuesto aparecerá aquí automáticamente...';
         return;
     }
 
-    let processedText = inputText;
-    if (useLatinNotation) {
-        processedText = convertNotation(inputText, true);
-    }
+    let processedText = inputText.replace(chordPattern, (match) => {
+        const transposedChord = transposeChord(match, currentTransposition);
+        return `<span class="chord">${transposedChord}</span>`;
+    });
 
-    const transposedText = transposeText(processedText, currentTransposition);
-
-    const chordPattern = useLatinNotation ?
-        /\b(Do|Re|Mi|Fa|Sol|La|Si)([#b]?)((?:maj|min|m|M|dim|aug|sus|add|\d)*(?:[#b]?\d+)*)\b/g :
-        /\b([A-G])([#b]?)((?:maj|min|m|M|dim|aug|sus|add|\d)*(?:[#b]?\d+)*)\b/g;
-
-    const highlightedText = transposedText.replace(chordPattern, '<span class="chord">$1$2$3</span>');
-
-    document.getElementById('outputText').innerHTML = highlightedText;
+    outputElement.innerHTML = processedText;
 }
 
 function transpose(semitones) {
     currentTransposition += semitones;
-    updateKeyDisplay();
+    updateCurrentKey();
     processText();
 }
 
 function quickTranspose(semitones) {
     currentTransposition += semitones;
-    updateKeyDisplay();
+    updateCurrentKey();
     processText();
+}
+
+function updateCurrentKey() {
+    const keyElement = document.getElementById('currentKey');
+    if (currentTransposition === 0) {
+        keyElement.textContent = 'Original';
+    } else {
+        const sign = currentTransposition > 0 ? '+' : '';
+        keyElement.textContent = `${sign}${currentTransposition}`;
+    }
 }
 
 function resetTransposition() {
     currentTransposition = 0;
-    updateKeyDisplay();
+    updateCurrentKey();
     processText();
 }
 
 function toggleNotation() {
-    useLatinNotation = !useLatinNotation;
-    updateNotationDisplay();
+    useEnglishNotation = !useEnglishNotation;
+    const notationText = document.getElementById('notationText');
+    const notationIndicator = document.getElementById('notationIndicator').querySelector('span');
+
+    if (useEnglishNotation) {
+        notationText.textContent = 'Inglesa (C D E)';
+        notationIndicator.textContent = 'Notación Inglesa';
+    } else {
+        notationText.textContent = 'Latina (Do Re Mi)';
+        notationIndicator.textContent = 'Notación Latina';
+    }
+
     processText();
 }
 
-function updateKeyDisplay() {
-    const keyDisplay = document.getElementById('currentKey');
-    if (currentTransposition === 0) {
-        keyDisplay.textContent = 'Original';
-    } else if (currentTransposition > 0) {
-        const tones = Math.floor(currentTransposition / 2);
-        const semitones = currentTransposition % 2;
-        let text = '+';
-        if (tones > 0) text += `${tones}t `;
-        if (semitones > 0) text += `${semitones}st`;
-        keyDisplay.textContent = text.trim();
-    } else {
-        const tones = Math.floor(Math.abs(currentTransposition) / 2);
-        const semitones = Math.abs(currentTransposition) % 2;
-        let text = '-';
-        if (tones > 0) text += `${tones}t `;
-        if (semitones > 0) text += `${semitones}st`;
-        keyDisplay.textContent = text.trim();
-    }
+function copyTransposedText() {
+    const outputElement = document.getElementById('outputText');
+    const textToCopy = outputElement.innerText;
+
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        showCopyNotification();
+    }).catch(() => {
+        const textArea = document.createElement('textarea');
+        textArea.value = textToCopy;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        showCopyNotification();
+    });
 }
 
-function updateNotationDisplay() {
-    const notationText = document.getElementById('notationText');
-    const notationIndicator = document.getElementById('notationIndicator');
-
-    if (useLatinNotation) {
-        notationText.textContent = 'Notación: Latina (Do Re Mi)';
-        notationIndicator.textContent = 'Notación Latina';
-    } else {
-        notationText.textContent = 'Notación: Inglesa (C D E)';
-        notationIndicator.textContent = 'Notación Inglesa';
-    }
+function showCopyNotification() {
+    const notification = document.getElementById('copyNotification');
+    notification.classList.add('show');
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
 }
 
-updateKeyDisplay();
-updateNotationDisplay();
+function clearText() {
+    document.getElementById('inputText').value = '';
+    document.getElementById('outputText').textContent = 'El texto transpuesto aparecerá aquí automáticamente...';
+}
