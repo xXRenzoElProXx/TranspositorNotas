afd8189d39fe62c1bf4187cac8e467b297c15f91
Renzo Crisanto <117412799+xXRenzoElProXx@users.noreply.github.com>
2025-06-10 10:03:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,37 @@ let useEnglishNotation = true;
 const englishChords = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 const latinChords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si'];
 
-const chordPattern = /\b(Do#|Re#|Fa#|Sol#|La#|Do|Re|Mi|Fa|Sol|La|Si|C#|D#|F#|G#|A#|C|D|E|F|G|A|B)(m|maj|min|sus|dim|aug|\d)*\b/g;
+const spanishWords = new Set([
+    'cómo', 'como', 'con', 'corazón', 'cuando', 'donde', 'dónde', 'solo', 'sólo',
+    'mi', 'mí', 'mis', 'mismo', 'misma', 'mientras', 'desde', 'después', 'dentro',
+    'ella', 'ellas', 'ellos', 'este', 'esta', 'estos', 'estas', 'favor', 'final',
+    'sobre', 'ser', 'sin', 'son', 'sus', 'hasta', 'hacia', 'hay', 'hoy', 'del',
+    'las', 'los', 'una', 'uno', 'para', 'por', 'pero', 'ser', 'estar', 'tener',
+    'hacer', 'decir', 'todo', 'cada', 'muy', 'bien', 'aquí', 'allí', 'más',
+    'menos', 'antes', 'ahora', 'siempre', 'nunca', 'también', 'solo', 'sólo',
+    'me', 'te', 'se', 'le', 'la', 'lo', 'al', 'el', 'de', 'da', 'si', 'sí',
+    'no', 're', 'fe', 'sol', 'lado', 'lado', 'lados', 'sos', 'eres', 'ere',
+    'era', 'eras', 'eso', 'esa', 'ese', 'esos', 'esas', 'son', 'somos',
+    'qué', 'que', 'quien', 'quién', 'quienes', 'cual', 'cuál', 'cuales', 'cuáles',
+    'adorar', 'amar', 'amor', 'amores', 'camino', 'caminos', 'falta', 'faltas',
+    'salud', 'saludo', 'saludos', 'mundo', 'mundos', 'tiempo', 'tiempos',
+    'miedo', 'miedos', 'vida', 'vidas', 'solamente', 'soledad', 'soldado',
+    'soldados', 'resolver', 'resolución', 'resoluciones', 'familia', 'familias',
+    'musical', 'musicales', 'música', 'músicas', 'músico', 'músicos',
+    'lado', 'lados', 'lado', 'lados', 'solar', 'solares', 'resolver',
+    'resolución', 'resoluciones', 'lado', 'lados', 'lado', 'lados'
+]);
+
+function normalizeText(text) {
+    return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+}
+
+function isChord(word) {
+    const normalized = normalizeText(word);
+    const lower = word.toLowerCase();
+    return !spanishWords.has(lower) &&
+        /^(Do#?|Re#?|Mi|Fa#?|Sol#?|La#?|Si|C#?|D#?|E|F#?|G#?|A#?|B)(m|maj|min|sus|dim|aug|\d*)?$/.test(word);
+}
 
 function getChordArray() {
     return useEnglishNotation ? englishChords : latinChords;
@@ -36,7 +66,6 @@ function transposeChord(chord, semitones) {
 
     return chord;
 }
-
 function processText() {
     const inputText = document.getElementById('inputText').value;
     const outputElement = document.getElementById('outputText');
@@ -46,9 +75,23 @@ function processText() {
         return;
     }
 
-    let processedText = inputText.replace(chordPattern, (match) => {
-        const transposedChord = transposeChord(match, currentTransposition);
-        return `<span class="chord">${transposedChord}</span>`;
+    const chordRegex = /\(([^)]+)\)|(?<=^|\s)(?:(Do#?|Re#?|Mi|Fa#?|Sol#?|La#?|Si|C#?|D#?|E|F#?|G#?|A#?|B)(m|maj|min|sus|dim|aug|\d*)?)(?=\s|[.,;!?)]|$)/g;
+
+    let processedText = inputText.replace(chordRegex, (match, group1, base, suffix) => {
+        if (group1) {
+            const chords = group1.trim().split(/\s+/).map(chord => {
+                return isChord(chord)
+                    ? `<span class="chord">${transposeChord(chord, currentTransposition)}</span>`
+                    : chord;
+            });
+            return `(${chords.join(' ')})`;
+        } else if (base) {
+            const fullChord = base + (suffix || '');
+            if (isChord(fullChord)) {
+                return `<span class="chord">${transposeChord(fullChord, currentTransposition)}</span>`;
+            }
+        }
+        return match;
     });
 
     outputElement.innerHTML = processedText;
